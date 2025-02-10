abcdc5aa69d507719c5220157e58c43c5763b7ec
Douglas Lima <165210202+limadouglas07@users.noreply.github.com>
2025-02-10 19:25:21 -0300

    Add files via upload

---
+++ oo-sabor-express/modelos/restaurante.py
@@ -0,0 +1,14 @@
+class Restaurante:
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+
+
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+# Colocando o vars listará os atributos e métodos.
+print(vars(restaurante_praca))
+print(vars(restaurante_pizza))
